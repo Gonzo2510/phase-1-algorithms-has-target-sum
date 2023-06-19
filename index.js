5c65b46f757b7d3f85214724db86e19f42c05baa
jacobgonzales <jagonz@live.com>
2023-06-19 16:50:42 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,18 +1,97 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+const { consoleLog } = require("mocha/lib/reporters/base")
+
+function hasTargetSum(array = [4], target = 4) {
+  //console.log(array.length)
+  for (let first = 0; first < array.length; first++) { //loop through array
+    if (array.length === 1) {
+    console.log(`there was only one item in the array, returning false`)
+    return false
+    } else if (array[first] > target){ //first number is bigger
+
+      if (first === array.length -1){ //last iteration
+        console.log('end of array was too big and was skipped, returning false')
+        return false
+      }
+
+      console.log(`${array[first]} first too big, continuing`) // first in array was too big, skipping to next iteration
+      continue 
+
+
+
+    } else if (array[first] < target){ //first is smaller
+        for (let second = first + 1; second < array.length; second++) { //get second number
+          let sum = array[first] + array[second] //add numbers together
+
+          if (sum === target) { //return true if equal target
+            console.log(`${array[first]} and ${array[second]} equal the target: true`)
+            return true
+          } else {
+            continue
+          }
+        }
+    } else if (first === array.length -1) {
+      console.log('last iteration false')
+      return false
+    }
+  }
 }
+hasTargetSum()
+
+
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+  // Add your pseudocode here
+
+loop though array
+  first number is bigger
+    if on last iteration
+      retun false
+    skip
+
+  first number is smaller
+    get second number
+      if second is bigger
+        skip 
+      second smaller
+        add numbers
+          if equal target
+            return true
+          if not 
+            continue
+        last iteration 
+          none match 
+            return false
+
+
+
+
+
+
+
+
+
+
+  write a loop to add up every pair of numbers
+  when adding a number check to see if it is less than the target number.
+    if less continue
+    else skip 
+  if a pair adds up to equal target return true
+  else return false
+
 */
 
 /*
   Add written explanation of your solution here
 */
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
